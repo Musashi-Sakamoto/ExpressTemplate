5873d96fdb226db83fe9e2fcb23e66230ef0642d
Musashi Sakamoto <1292602b@gmail.com>
2020-04-19 10:27:03 -0700

    release にむけた設定

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 
+var compression = require('compression');
+var helmet = require('helmet');
+
 var app = express();
+
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +30,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
